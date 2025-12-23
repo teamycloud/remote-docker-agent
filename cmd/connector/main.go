ba756f63986777a4b1c6df28c20f3e5681fb0695
Jijie Chen <1180092+jijiechen@users.noreply.github.com>
2025-12-24 00:22:35 +0800

    添加了基本的多应用结构

---
+++ cmd/connector/main.go
@@ -0,0 +1,6 @@
+package main
+
+
+// handle HTTP requests and execute the following:
+// mtls check
+// forward requests to upstream server (by query database for SNI to upstream mapping)
---
+++ cmd/guest/main.go
@@ -0,0 +1,6 @@
+package main
+
+
+// handle HTTP requests and execute the following:
+// command - upgrade the connection and pipe process stdin/stdout to the connection
+// copy - receive data from the connection and write to a file
